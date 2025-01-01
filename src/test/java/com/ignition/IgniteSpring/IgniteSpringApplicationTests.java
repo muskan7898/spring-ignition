085682937ff3415c9e3fcd0d1c97d3f32c1432cf
muskan7898 <muskannareliya7@gmail.com>
2025-01-01 18:19:52 +0530

    first commit

---
+++ src/test/java/com/ignition/IgniteSpring/IgniteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignition.IgniteSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
