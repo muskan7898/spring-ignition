cd14aa70b9970a8dbe903ccebf284cf1594c26cd
muskan7898 <muskannareliya7@gmail.com>
2025-01-14 15:24:39 +0530

    add some apis

--- src/main/java/com/ignition/IgniteSpring/controller/VehicleBookingController.java
+++ src/main/java/com/ignition/IgniteSpring/controller/VehicleBookingController.java
@@ -12,4 +12,5 @@ import org.springframework.web.bind.annotation.*;
 public class VehicleBookingController {
     private final VehicleBookingService vehicleBookingService;
 
+
 }
---
+++ src/main/java/com/ignition/IgniteSpring/dto/res/VehicleDetail.java
@@ -0,0 +1,5 @@
+package com.ignition.IgniteSpring.dto.res;
+
+public class VehicleDetail {
+
+}
